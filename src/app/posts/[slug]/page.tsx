43674d4530fdb702de0bd20c54dba8815941159c
Shashwat Kansal <skansal41@gmail.com>
2024-09-15 14:56:53 +0100

    refactor: Update page title for London Hub III article

--- src/app/posts/[slug]/page.tsx
+++ src/app/posts/[slug]/page.tsx
@@ -58,7 +58,7 @@ export function generateMetadata({ params }: Params): Metadata {
     return notFound();
   }
 
-  const title = `${post.title} | Next.js Blog Example with ${CMS_NAME}`;
+  const title = `${post.title} | Global Shapers London Hub III Article`;
 
   return {
     title,
